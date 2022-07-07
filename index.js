066bfa2d6ac75f186faeb09180708678d850ca4f
NicknameIsAvailable <iamgenii@yandex.ru>
2022-07-07 20:39:24 +0300

    added test auth and mongodb connection

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 import express from 'express'
 import jwt from 'jsonwebtoken'
+import mongoose from 'mongoose'
+
+mongoose
+    .connect('mongodb+srv://admin:12345@cluster0.dj8vu.mongodb.net/?retryWrites=true&w=majority')
+    .then(() => console.log('DB ok'))
+    .catch((err) => console.log('DB error', err))
 
 const app = express()
 
