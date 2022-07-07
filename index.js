14249be478bd5bdc4610ccadaf6896b10931a0d0
NicknameIsAvailable <iamgenii@yandex.ru>
2022-07-07 18:35:15 +0300

    test auth

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-console.log('скоро тут что-нибудь будет, жди')
+import express from 'express'
+import jwt from 'jsonwebtoken'
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('hello world')
+})
+
+app.post('/auth/login', (req, res) => {
+
+    console.log(req.body)
+
+    const token = jwt.sign({
+        email: req.body.email,
+        fullName: "Василий Пупкин"
+    }, 'OqvDM42wYbwSlfpO')
+
+    res.json({
+        success: true,
+        token
+    })
+})
+
+app.listen(4444, (err) => {
+    if (err) {
+        return console.log(err)
+    }
+    console.log('Server OK')
+})
+
