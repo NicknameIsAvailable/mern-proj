56b40bdc2363a7f902f65670b62ac755004c60c0
NicknameIsAvailable <iamgenii@yandex.ru>
2022-07-09 13:06:54 +0300

    first commit

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 import express from 'express'
 import jwt from 'jsonwebtoken'
 import mongoose from 'mongoose'
+import {validationResult} from "express-validator";
+import bcrypt from 'bcrypt'
+import {registerValidator} from "./validations/auth.js"
+import User from "./models/User.js"
 
 mongoose
-    .connect('mongodb+srv://admin:12345@cluster0.dj8vu.mongodb.net/?retryWrites=true&w=majority')
+    .connect('mongodb+srv://admin:12345@cluster0.dj8vu.mongodb.net/blog?retryWrites=true&w=majority')
     .then(() => console.log('DB ok'))
     .catch((err) => console.log('DB error', err))
 
@@ -11,23 +15,73 @@ const app = express()
 
 app.use(express.json())
 
+app.post('/auth/login', async (req, res) => {
+    try {
+        const user = await UserModel.findOne({email: req.body.email})
+
+        if (!user) {
+            return req.status(404).json({
+                message: 'Пользователь не найден'
+            })
+        }
+
+        const isValidPass = await bcrypt.compare(req.body.password, use._doc.passwordHash)
+
+        if(!isValidPass) {
+            return req.status(404).json({
+                message: 'Неверный логин или пароль'
+            })
+        }
+    } catch (err) {
+
+    }
+})
+
 app.get('/', (req, res) => {
     res.send('hello world')
 })
 
-app.post('/auth/login', (req, res) => {
+app.post('/auth/register', async (req, res) => {
 
-    console.log(req.body)
+    try {
 
-    const token = jwt.sign({
-        email: req.body.email,
-        fullName: "Василий Пупкин"
-    }, 'OqvDM42wYbwSlfpO')
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+            return res.status(400).json(errors, array())
+        }
 
-    res.json({
-        success: true,
-        token
-    })
+        const password = req.body.password
+        const salt = await bcrypt.genSalt(10)
+        const hash = await bcrypt.hash(password, salt)
+
+        const doc = new User({
+            email: req.body.email,
+            fullName: req.body.fullName,
+            passwordHash: req.body.password,
+            avatarUrl: req.body.avatarUrl
+        })
+
+        const user = await doc.save()
+
+        const token = jwt.sign({
+            _id: user._id,
+        }, '~A|1Q5m5ki7Gg4za',
+            {
+                expiresIn: '30d'
+            })
+
+        const {passwordHash, ...userData} = user._doc
+
+        res.json({
+            ...userData, token
+        })
+
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({
+            message: "Не удалось зарегистрироваться",
+        })
+    }
 })
 
 app.listen(4444, (err) => {
---
+++ validations/auth.js
@@ -0,0 +1,8 @@
+import {body} from 'express-validator'
+
+export const registerValidator = [
+    body('email', 'неверный формат почты').isEmail(),
+    body('password', 'неверный формат пароля').isLength({min: 5}),
+    body('fullName', 'укажите имя').isLength({min: 3}),
+    body('avatarUrl', 'неверная ссылка на аватарку').optional().isURL()
+]
