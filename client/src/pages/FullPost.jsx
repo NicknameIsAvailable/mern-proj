c3eede4c498b4f5d43e4925d95c697dd6ce6e6fa
NicknameIsAvailable <iamgenii@yandex.ru>
2022-07-21 20:01:04 +0300

    Деплой на vercel

--- client/src/pages/FullPost.jsx
+++ client/src/pages/FullPost.jsx
@@ -33,7 +33,7 @@ export const FullPost = () => {
             <Post
                 id={data._id}
                 title={data.title}
-                imageUrl={data.imageUrl ? `http://localhost:4444${data.imageUrl}` : ''}
+                imageUrl={data.imageUrl ? `${process.env.REACT_APP_API_URL}/${data.imageUrl}` : ''}
                 user={data.user}
                 createdAt={data.createAt}
                 viewsCount={data.viewsCount}
