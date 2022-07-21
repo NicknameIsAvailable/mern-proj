a0de51c0a597db55481f4974670a3b70f888eb0e
NicknameIsAvailable <iamgenii@yandex.ru>
2022-07-21 19:50:19 +0300

    Деплой на Heroku

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ import { handleValidationErrors, checkAuth } from './utils/index.js';
 import { userController, postController } from './controllers/index.js';
 
 mongoose
-    .connect('mongodb+srv://admin:12345@cluster0.dj8vu.mongodb.net/blog?retryWrites=true&w=majority')
+    .connect(process.env.MONGODB_URI)
     .then(() => console.log('DB ok'))
     .catch((err) => console.log('DB error', err))
 
